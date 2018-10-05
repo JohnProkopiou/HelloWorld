1b6e1c15120168e0500e951fd0a66d5fca79c6cc
JohnProkopiou <43877728+JohnProkopiou@users.noreply.github.com>
2018-10-05 13:00:59 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld{
 public static void main(String [] args) {
-	System.out.println("Hello World! ")
+	System.out.println("Hello World! ");
+	System.out.println("This is me!");
+	System.out.println("Life should be!");
+	System.out.println("Fun fun everyone!");
 	}
 }
